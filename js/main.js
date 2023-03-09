fa023343efe898d8bce24fcd10d55cc91b870875
Mayank Jamwal <jamwalmayank75@gmail.com>
2023-03-09 17:46:19 +0530

    Todo list app

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const submitBtn = document.querySelector('.submit');
+const clearBtn = document.querySelector('.clear');
+const userInput = document.querySelector('.userText');
+
+
+submitBtn.addEventListener('click',submitUserData);
+clearBtn.addEventListener('click',clearUserData);
+
+function submitUserData(){
+
+    
+}
+
+function clearUserData(){
+    
+}
