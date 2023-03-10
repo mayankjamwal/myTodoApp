813091c394b8c2b2dbea9630a0eca3c3b67e9656
Mayank Jamwal <jamwalmayank75@gmail.com>
2023-03-11 01:30:42 +0530

    Adding js to enable the buttons

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
-const submitBtn = document.querySelector('.submit');
-const clearBtn = document.querySelector('.clear');
-const userInput = document.querySelector('.userText');
+console.log("runn...");
+const submitBtn = document.getElementsByClassName("submit")[0];
+const clearBtn = document.getElementsByClassName("clear")[0];
+const userInput = document.getElementsByClassName("userText")[0];
+const head = document.getElementsByClassName("heading")[0];
+// console.log(head);
+// console.log(submitBtn);
 
+submitBtn.addEventListener("click",submitUserData);
+clearBtn.addEventListener("click",clearUserData);
 
-submitBtn.addEventListener('click',submitUserData);
-clearBtn.addEventListener('click',clearUserData);
 
 function submitUserData(){
-
-    
+  const li = document.createElement('li');
+  li.textContent = userInput.value;
+  const ul = document.querySelector('.ul-list');
+  ul.appendChild(li);
+  userInput.value = ''; 
 }
 
 function clearUserData(){
+    const li = document.querySelector("li");
+    userInput.value = '';
+    if(li){
+        li.remove();
+    }
+    else{
+        console.error("Error: No list Item is present.");
+    }
     
 }
+
